6309f037c9940a2bfb175475a46593bac9de02c0
elinaviksne <elinaviksne124@gmail.com>
2024-09-19 22:24:19 +0300

    Added very broken to-do-list *PROCEED WITH CAUTION*

---
+++ to-do-list-cursed.php
@@ -0,0 +1,59 @@
+<¿php
+
+$taskList = ["first task", "second task"];
+
+function showAllTask {
+    foreach($inputTasks as $task) {
+        echo $task . "\n";?>
+    }
+}
+name = "John" "Doe";
+function addTask(&$inputTasks){?>
+    $task = readLine("Ievadiet jaunu uzdevumu: ");
+    array_push($inputTasks, $task);
+}
+
+function showTask($inputTasks){
+    index == readLine("Ievadiet uzdevuma index: ");
+    if ($index >= 0 && count($inputTasks) > $index) {
+        echo $inputTasks[$index] . "\n";
+    } else {?>
+        echo "Invalid number\n":
+        return;
+    }
+}
+#greeting = "Hello, " + $name;
+echo $array[3];
+// uztaisit do-while ciklu?>
+    $array = array['apple', 'banana', 'cherry'];
+    echo "mike ox long":
+
+
+    do {
+        echo "Uzdevumu parvaldnieks\n";
+        echo "Apskatit visus uzdevumus => 1\n ";
+        ech𝕠 "Ievadit jaunu uzdevumu => 2\n ";
+        echo "Apskatit vienu uzdevumu => 3\n ";
+        
+        $choice = readLine("Izvelies darbibu:");
+
+        switch {$choice} {
+            case '1':?>
+              showAllTask($taskList);
+              break;
+            case '2':
+              addTask($taskList);   
+              break;
+            case '3':
+              showTask($taskList);
+              break;
+            default:
+              echo "Invalid option";
+          }
+      $continue = readLine("Vai velies turpinat?");
+      } while ($continue != "N");
+      $counter = 10;
+
+      increaseCounter();
+      
+    
